bd03f62810b84d6decb046b62cf5431327388d95
pruk.n <pruk.n@K17SOFD3583400.kasikornbank.com>
2018-06-26 15:31:33 +0700

    [Edited]: สร้าง api http://localhost:8888/duration

--- src/api/datecalculatehandle.go
+++ src/api/datecalculatehandle.go
@@ -1,1 +1,37 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// our main function
+
+func Calculate(w http.ResponseWriter, r *http.Request) {
+
+	var resp Response
+	//fmt.Printf(r.URL.Query().Get("point"))
+	//resp.Point = r.URL.Query().Get("point")
+	//resp.Grade = grade
+	//resp.
+	resp.Humanreadday = "hahah"
+	json.NewEncoder(w).Encode(resp)
+	return
+}
+
+type Request struct {
+	StartDate string `json:"startdate,omitempty"`
+	EndDate   string `json:"enddate,omitempty"`
+}
+
+type Response struct {
+	From          string `json:"from,omitempty"`
+	To            string `json:"to,omitempty"`
+	Totalday      string `json:"to,omitempty"`
+	Humanreadday  string `json:"humanreadday,omitempty"`
+	Second        string `json:"second,omitempty"`
+	Minute        string `json:"minute,omitempty"`
+	Hour          string `json:"hour,omitempty"`
+	Week          string `json:"week,omitempty"`
+	Percentofyear string `json:"percentofyear,omitempty"`
+}
--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"api"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/duration", api.Calculate).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8888", router))
+
+}
