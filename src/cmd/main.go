1dfcc4a62e22ee67519e0c92ad12446286ee4cf9
pruk.n <pruk.n@K17SOFD3583400.kasikornbank.com>
2018-06-27 00:55:12 +0700

    add bootstrap template

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/duration", api.Calculate).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8888", router))
+	fs := http.FileServer(http.Dir("./html"))
+	router.PathPrefix("/").Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	})).Methods("GET")
 
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
