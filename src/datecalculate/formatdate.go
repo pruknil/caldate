ce6eb3e88b12fb9774418ae52f679014bc6309d9
natsanth <37365875+natsanth@users.noreply.github.com>
2018-06-27 09:59:12 +0700

    [Edited]: program formatdate.go and fomratdate_test.go

--- src/datecalculate/formatdate.go
+++ src/datecalculate/formatdate.go
@@ -7,10 +7,6 @@ import (
 )
 
 func FormatDate(inputDate string) string {
-	//day, _ := strconv.Atoi(inputDate[0:2])
-	//month, _ := strconv.Atoi(inputDate[2:4])
-	//year, _ := strconv.Atoi(inputDate[4:8])
-
 	day, month, year := convertDate(inputDate)
 	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 
--- src/datecalculate/formatdate_test.go
+++ src/datecalculate/formatdate_test.go
@@ -2,10 +2,12 @@ package datecalculate
 
 import "testing"
 
-func TestFormatDateInput412018ShouldBeThurday4January2018(t *testing.T) {
+func TestFormatDateInput04012018ShouldBeThursday4January2018(t *testing.T) {
 	expected := "Thursday, 4 January 2018"
 	startDate := "04012018"
+
 	actualDate := FormatDate(startDate)
+
 	if actualDate != expected {
 		t.Errorf("expected %s but got %s", expected, actualDate)
 	}
