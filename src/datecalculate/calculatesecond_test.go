196db48b46faaa0ad7964a88b5bbb028948b370b
bigsupanat <bigsupanat@gmail.com>
2018-06-27 11:59:38 +0700

    [Edited]: แก้จาก expexted เป็น expected

--- src/datecalculate/calculatesecond_test.go
+++ src/datecalculate/calculatesecond_test.go
@@ -4,11 +4,10 @@ import "testing"
 
 func TestCalculateSecond182Shouldbe15724800(t *testing.T) {
 	days := 182
-	expexted := "15,724,800 Seconds"
-
+	expected := "15,724,800 Seconds"
 	actual := calculateSecond(days)
-	if expexted != actual {
-		t.Errorf("Expected: %s Actual: %s",expexted,actual)
+	if expected != actual {
+		t.Errorf("Expected: %s Actual: %s", expected, actual)
 	}
 
 }
